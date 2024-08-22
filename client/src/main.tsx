1b3097c9b1a7824d4771420b7dcf02e59bacd22f
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-22 13:29:47 +0300

    improtr mantine

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,11 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './App/providers/store/store.ts';
 
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </Provider>
 );
