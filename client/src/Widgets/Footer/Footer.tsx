e61b3931ef40a394d4c2ad598d6d3a6560fe4454
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-22 16:35:01 +0300

    finishing footer

--- client/src/Widgets/Footer/Footer.tsx
+++ client/src/Widgets/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Group, rem } from '@mantine/core';
+import { Group, rem, Anchor, Text } from '@mantine/core';
 import { IconBrandGithub } from '@tabler/icons-react';
 
 import classes from './FooterCentered.module.css';
@@ -8,10 +8,23 @@ export function Footer() {
     <div className={classes.footer}>
       <div className={classes.inner}>
         <Group className={classes.links} gap="xs">
-          <IconBrandGithub
-            style={{ width: rem(18), height: rem(18) }}
-            stroke={1.5}
-          />
+          <Text className={classes.title} >
+            Developed by:
+          </Text>
+          <Text>Igor</Text>
+          <Anchor href="https://github.com/IgorPetrov-13" target="_blank">
+            <IconBrandGithub
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </Anchor>
+          <Text>Tamirlan</Text>
+          <Anchor href="https://github.com/tomq29" target="_blank">
+            <IconBrandGithub
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </Anchor>
         </Group>
       </div>
     </div>
