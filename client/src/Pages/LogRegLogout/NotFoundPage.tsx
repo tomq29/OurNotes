79a0c5818dc8f5bd3143f7b6508f6d5c4bae0764
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-08-22 20:42:14 +0300

    feat: main_layout done

--- client/src/Pages/LogRegLogout/NotFoundPage.tsx
+++ client/src/Pages/LogRegLogout/NotFoundPage.tsx
@@ -1,26 +1,25 @@
-
-
 function NotFoundPage(): JSX.Element {
   return (
-    <div 
+    <div
       className="container-fluid d-flex flex-column justify-content-center align-items-center"
       style={{
-        height: '100vh',
-        textAlign: 'center',
-        backgroundColor: '#f8f9fa',
-        color: '#343a40',
+        height: "100vh",
+        textAlign: "center",
       }}
     >
-      <h1 style={{ fontSize: '4rem', fontWeight: 'bold', marginBottom: '1rem' }}>Потерялся 404 раза?</h1>
-      <h2 style={{ fontSize: '2rem', marginBottom: '2rem' }}>Иди обниму</h2>
+      <h1
+        style={{ fontSize: "4rem", fontWeight: "bold", marginBottom: "1rem" }}
+      >
+        Потерялся 404 раза?
+      </h1>
+      <h2 style={{ fontSize: "2rem", marginBottom: "2rem" }}>Иди обниму</h2>
       <img
         src="/404.gif"
         alt="Not Found"
         style={{
-          maxWidth: '100%',
-          height: 'auto',
-          borderRadius: '8px',
-          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)',
+          maxWidth: "100%",
+          height: "auto",
+          borderRadius: "8px",
         }}
       />
     </div>
