5c7f2c8d05bccba59833e6f389766c7c7c944aff
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-08-23 19:34:32 +0300

    fix: according review 23.08

--- client/src/App/providers/router/AppRouter.tsx
+++ client/src/App/providers/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from '../../../Pages/HomePage/HomePage';
 import QuestionsPage from '../../../Pages/QuestionsPage/QuestionsPage';
 import QuotesPage from '../../../Pages/QuotesPage/QuotesPage';
@@ -10,8 +10,45 @@ import NotFoundPage from '../../../Pages/LogRegLogout/NotFoundPage';
 import NotesPage from '../../../Pages/NotesPage/NotesPage';
 import OneNotePage from '../../../Pages/OneNotePage/OneNotePage';
 import ProfilePage from '../../../Pages/Profile/ProfilePage';
+import { useEffect } from 'react';
 
 function AppRouter(): JSX.Element {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Устанавливаем заголовок страницы в зависимости от текущего пути
+    switch (location.pathname) {
+      case '/':
+        document.title = 'Our Notes';
+        break;
+      case '/notes':
+        document.title = 'Заметки';
+        break;
+      case '/questions':
+        document.title = 'Вопросы';
+        break;
+      case '/profile':
+        document.title = 'Личный кабинет';
+        break;
+      case '/quotes':
+        document.title = 'Цитаты';
+        break;
+      case '/objectwriting':
+        document.title = 'Objectwriting';
+        break;
+      case '/auth/login':
+        document.title = 'Выход';
+        break;
+      case '/auth/logout':
+        document.title = 'Выход';
+        break;
+      case '/auth/reg':
+        document.title = 'Регистрация';
+        break;
+      default:
+        document.title = 'Our Notes';
+    }
+  }, [location]);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
