6738df8e8dc91be119ad09bc0b07fa1fcd6cafdf
kokomimainr <akmatovamantur097@gmail.com>
2024-08-26 16:33:38 +0300

    feat/LogoutPage and NavBar fixed

--- client/src/Pages/LogRegLogout/LogOutPage.tsx
+++ client/src/Pages/LogRegLogout/LogOutPage.tsx
@@ -1,10 +1,12 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mantine/core";
+import { Title, Container} from "@mantine/core";
 
 import {
   useAppDispatch,
   useAppSelector,
-} from '../../App/providers/store/store';
-import { logoutUser } from '../../Entities/User/model/CurrentUserSlice';
+} from "../../App/providers/store/store";
+import { logoutUser } from "../../Entities/User/model/CurrentUserSlice";
 
 function LogOutPage(): JSX.Element {
   const currentUser = useAppSelector((state) => state.currentUserStore.user);
@@ -14,33 +16,42 @@ function LogOutPage(): JSX.Element {
 
   const logoutHandler = () => {
     dispatch(logoutUser())
-      .then(() => navigate('/'))
+      .then(() => navigate("/"))
       .catch(console.log);
   };
 
   return (
     <>
       <div className="vstack gap-2 col-md-5 mx-auto">
-        <div> {currentUser?.login}, Вы точно хотите выйти?</div>
-        <button
-          onClick={logoutHandler}
-          type="button"
-          className="btn btn-outline-secondary"
-        >
-          Да!
-        </button>
-        <button
-          onClick={() => navigate('/')}
-          type="button"
-          className="btn btn-outline-success"
-        >
-          Нет, хочу остаться
-        </button>
+        <Container size={420} my={100}>
+          <Title ta="center"> {currentUser?.login}, Вы точно хотите выйти?</Title>
+          <div className="buttons" >
+            <Button
+              color="red"
+              onClick={logoutHandler}
+              variant="light"
+              type="button"
+              className="btn btn-outline-secondary"
+              fullWidth mt='sm'
+              size="md"
+            >
+              Да!
+            </Button>
+            <Button
+              onClick={() => navigate("/")}
+              type="button"
+              variant="default"
+              className="btn btn-outline-success"
+              fullWidth mt='sm'
+              size="md"
+            >
+              Нет, хочу остаться
+            </Button>
+          </div>
+        </Container>
       </div>
     </>
   );
-
-
 }
 
 export default LogOutPage;
--- client/src/Widgets/NavBar/ui/NavBarForAnon.tsx
+++ client/src/Widgets/NavBar/ui/NavBarForAnon.tsx
@@ -1,30 +1,61 @@
-import { Group, Button, Box } from '@mantine/core';
+import {  Group,  Button, Divider, Box,  Burger,  Drawer,  ScrollArea,  rem } from '@mantine/core';
 import classes from '../HeaderMegaMenu.module.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ThemeToggle } from '../../../Shared/ThemeToggle/ThemeToggle';
+import { useDisclosure } from '@mantine/hooks';
 
 function NavBarForAnon(): JSX.Element {
   const navigate = useNavigate();
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
   return (
     <Box>
       <header className={classes.header}>
-        <Group justify="space-around" h="100%">
-          <Group h="100%" visibleFrom="sm">
-            <NavLink to={'/'} className={classes.link}>
+        <Group justify='space-around' h="100%">
+
+          <Group h="100%" gap={0} visibleFrom="sm">
+          <NavLink to={'/'} className={classes.link}>
               Главная
             </NavLink>
           </Group>
 
           <Group visibleFrom="sm">
-            <ThemeToggle />
-
             <Button onClick={() => navigate('/auth/login')} variant="default">
+                Авторизация
+              </Button>
+              <Button onClick={() => navigate('/auth/reg')}>Регистрация</Button>
+              <ThemeToggle />
+          </Group>
+          <Burger mr={1000} opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+        </Group>
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Навигация"
+        hiddenFrom="sm"
+        zIndex={1000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+
+          <NavLink to={'/'} className={classes.link}>
+              Главная
+            </NavLink>
+
+          <Divider my="sm" />
+
+          <Group justify="center" grow pb="xl" px="md">
+          <Button onClick={() => navigate('/auth/login')} variant="default">
               Авторизация
             </Button>
             <Button onClick={() => navigate('/auth/reg')}>Регистрация</Button>
+              <ThemeToggle />
           </Group>
-        </Group>
-      </header>
+        </ScrollArea>
+      </Drawer>
     </Box>
   );
 }
