c904d3f01aa04be7bcefc2b1480d57c72c815eb0
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-29 08:03:45 +0300

    save changes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -6,6 +6,7 @@ const indexRouter = require('express').Router();
 indexRouter.use('/api', apiRouter);
 indexRouter.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/dist/index.html'));
+  // res.send('hello');
 });
 
 module.exports = indexRouter;
