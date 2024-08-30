939181756155bd3288fd13e843890968e4d63514
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-30 13:05:04 +0300

    cleaning vite config and app.js in server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxying WebSocket connections to the Socket.IO server
-      '/socket.io': {
-        target: 'http://localhost:3000', // Replace with your actual server URL
-        ws: true, // Proxy WebSockets
-        changeOrigin: true,
-      },
       '/api': {
         target: 'http://localhost:3000/',
         changeOrigin: true,
--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const http = require('http'); // Import Node's HTTP module
-const socketIo = require('socket.io'); // Import Socket.IO
 
 const serverConfig = require('./config/serverConfig');
-const indexRouter = require('./routes/index.routes');
-const websocketHandler = require('./sockets/websocketHandler');
+
+const apiRouter = require('./routes/api/api.routes');
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -15,27 +14,10 @@ const app = express();
 serverConfig(app);
 
 //
-app.use('/', indexRouter);
-
+app.use('/api', apiRouter);
 
 //
 const server = http.createServer(app);
-
-const io = socketIo(server);
-
-io.on('connection', (socket) => {
-  // Set up WebSocket connection using y-websocket utilities
-  // setupWSConnection(socket);
-  console.log('A user connected');
-
-  socket.on('disconnect', () => {
-      console.log('User disconnected');
-  });
-});
-
-
-
-
 
 server.listen(PORT, () => {
   console.log(`Server starter at ${PORT} port`);
