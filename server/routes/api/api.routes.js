cdd35b9deac29cedb315ead66a1456086095b669
Tamirlan Kussainov <proxyuser@duck.com>
2026-09-11 00:06:45 +0500

    chore(server): remove unused folder and text endpoints
    Neither is called by the client and both were unauthenticated.

--- server/routes/api/api.routes.js
+++ server/routes/api/api.routes.js
@@ -1,8 +1,6 @@
 const authRouter = require('./auth.routes');
 const tokenRouter = require('./token.routes');
-const textRouter = require('./notesText.routes');
 const notesRouter = require('./notes.routes');
-const folderRouter = require('./folder.routes');
 const pairsRouter = require('./pairs.routes');
 const colorsRouter = require('./colors.routes');
 const usersRouter = require('./users.routes');
@@ -14,8 +12,6 @@ const apiRouter = require('express').Router();
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/tokens', tokenRouter);
 apiRouter.use('/notes', notesRouter);
-apiRouter.use('/folders', folderRouter);
-apiRouter.use('/text', textRouter);
 apiRouter.use('/pairs', pairsRouter);
 apiRouter.use('/colors', colorsRouter);
 apiRouter.use('/users', usersRouter);
